52efcaa4dad7e84f7c98c6d544672738234bb3ab
brianfung420_home <brianfung420@gmail.com>
2020-06-07 01:03:24 +0800

    add location information

--- app/src/main/java/com/example/Location_Final/DiceFragment.java
+++ app/src/main/java/com/example/Location_Final/DiceFragment.java
@@ -40,7 +40,7 @@ public class DiceFragment extends Fragment {
     double latitude = 120.646538;
     double longitude = 24.178843;
 
-    private HashMap<String,String> mData = new HashMap<>();
+    private List<HashMap<String,String>> mData;
     private RecyclerView recyclerView;
     private diceAdapter adapter;
     private Button btn_dice;
@@ -53,35 +53,145 @@ public class DiceFragment extends Fragment {
         btn_dice = RootView.findViewById(R.id.btn_diceRandom);
 
         recyclerView = RootView.findViewById(R.id.Dice_RecyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         LoadMapsInfo loadMapsInfo = new LoadMapsInfo();
         mData = loadMapsInfo.excute_PlacesTask();
 
-        if(mData.isEmpty()) {
+        if(mData==null) {
+            mData = new ArrayList<HashMap<String, String>>();
             Log.d("OnCreate","Is null");
-            mData.put("name"+"0","127");
-            mData.put("descr"+"0","咖喱飯");
+            HashMap<String,String> temp = new HashMap<>();
 
-            mData.put("name"+"1","七味");
-            mData.put("descr"+"1","便當");
+            temp.put("name","Little Tibet");
+            temp.put("lat","24.175309");
+            temp.put("long","120.64681");
+            mData.add(0,temp);
 
-            mData.put("name"+"2","123");
-            mData.put("descr"+"2","便當");
+            temp = new HashMap<String,String>();
+            temp.put("name","Mr. Chicken Head");
+            temp.put("lat","24.181797");
+            temp.put("long","120.644607");
+            mData.add(1,temp);
 
-            mData.put("name"+"3","456");
-            mData.put("descr"+"3","便當");
+            temp = new HashMap<String,String>();
+            temp.put("name","Cafe Buddha 佈達咖啡");
+            temp.put("lat","24.17674199999999");
+            temp.put("long","120.645502");
+            mData.add(2,temp);
 
-            mData.put("name"+"4","789");
-            mData.put("descr"+"4","便當");
+            temp = new HashMap<String,String>();
+            temp.put("name","Yixin Vegetarian Stinky Tofu");
+            temp.put("lat","24.1778562");
+            temp.put("long","120.6451825");
+            mData.add(3,temp);
 
-            mData.put("name"+"5","159");
-            mData.put("descr"+"5","便當");
+            temp = new HashMap<String,String>();temp.put("name","Mr.38 The Beat Curry");
+            temp.put("lat","24.1769469");
+            temp.put("long","120.6433179");
+            mData.add(4,temp);
 
-            mData.put("name"+"6","753");
-            mData.put("descr"+"6","便當");
+            temp = new HashMap<String,String>();temp.put("name","樂樂和風食堂-台中青海店");
+            temp.put("lat","24.171686");
+            temp.put("long","120.643981");
+            mData.add(5,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","官芝霖大腸包小腸");
+            temp.put("lat","24.1788954");
+            temp.put("long","120.6456488");
+            mData.add(6,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","In Sky Hotel");
+            temp.put("lat","24.1828703");
+            temp.put("long","120.6450211");
+            mData.add(7,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","梅香小吃");
+            temp.put("lat","24.180263");
+            temp.put("long","120.645765");
+            mData.add(8,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","陶板屋 台中河南店");
+            temp.put("lat","24.1715678");
+            temp.put("long","120.6447569");
+            mData.add(9,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","Plant Plan");
+            temp.put("lat","24.1711714");
+            temp.put("long","120.6476609");
+            mData.add(10,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","联亭泡菜鍋-逢甲店");
+            temp.put("lat","24.1827307");
+            temp.put("long","120.6447184");
+            mData.add(11,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","三號創意料理廚房");
+            temp.put("lat","24.1765332");
+            temp.put("long","120.6446064");
+            mData.add(12,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","激旨燒き鳥Gekiuma Yakitori 台灣總店");
+            temp.put("lat","24.1831289");
+            temp.put("long","120.6466054");
+            mData.add(13,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","龍涎居雞膳食坊 台中逢甲店");
+            temp.put("lat","24.1795836");
+            temp.put("long","120.6451754");
+            mData.add(14,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","長谷川壽司專賣店");
+            temp.put("lat","24.1715984");
+            temp.put("long","120.64594");
+            mData.add(15,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","台南鍋燒麵");
+            temp.put("lat","24.1755365");
+            temp.put("long","120.6449622");
+            mData.add(16,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","鍋神日式涮涮鍋");
+            temp.put("lat","24.1835551");
+            temp.put("long","120.6446369");
+            mData.add(17,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","韓石館");
+            temp.put("lat","24.1767214");
+            temp.put("long","120.6426575");
+            mData.add(18,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","月島文字燒 (台中逢甲店)");
+            temp.put("lat","24.174742");
+            temp.put("long","120.6468268");
+            mData.add(19,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","七味厨坊");
+            temp.put("lat","24.179503");
+            temp.put("long","120.646416");
+            mData.add(20,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","無名麻辣鴨血 逢甲店");
+            temp.put("lat","24.181244");
+            temp.put("long","120.646336");
+            mData.add(21,temp);
+
         }
 
+        Log.d("DiceFragmet","list size:"+mData.size());
         // 將資料交給adapter
         adapter = new diceAdapter(mData);
         // 設置adapter給recycler_view
@@ -91,7 +201,7 @@ public class DiceFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Random random = new Random();
-                int count = random.nextInt((mData.size()/2));
+                int count = random.nextInt((mData.size()));
                 Log.d("btn_dice","Clicked,Random Number:"+count+" ,mData.size():"+mData.size());
             }
         });
--- app/src/main/java/com/example/Location_Final/MapsFragment.java
+++ app/src/main/java/com/example/Location_Final/MapsFragment.java
@@ -21,45 +21,158 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     SupportMapFragment mapFragment;
     private GoogleMap mMap;
-    HashMap<String,String> restartant_info = new HashMap<>();
+    private List<HashMap<String,String>> restartant_info;
 
     public MapsFragment() {
         // Required empty public constructor
+        restartant_info  = new ArrayList<HashMap<String, String>>();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LoadMapsInfo loadMapsInfo = new LoadMapsInfo();
+        LoadMapsInfo loadMapsInfo = new LoadMapsInfo(24.179231,120.649661);
+
         restartant_info = loadMapsInfo.excute_PlacesTask();
 
-        if(restartant_info.isEmpty()){
+        if(restartant_info==null){
             Log.d("OnCreate","Is null");
-            restartant_info.put("name"+"0","127");
-            restartant_info.put("descr"+"0","咖喱飯");
-            restartant_info.put("lat"+"0","24.180014");
-            restartant_info.put("long"+"0","120.645990");
+            restartant_info  = new ArrayList<HashMap<String, String>>();
+            HashMap<String,String> temp = new HashMap<String,String>();
 
+            temp.put("name","Little Tibet");
+            temp.put("lat","24.175309");
+            temp.put("long","120.64681");
+            restartant_info.add(0,temp);
 
-            restartant_info.put("name"+"1","七味厨坊");
-            restartant_info.put("descr"+"1","便當");
-            restartant_info.put("lat"+"1","24.179503");
-            restartant_info.put("long"+"1","120.646416");
+            temp = new HashMap<String,String>();
+            temp.put("name","Mr. Chicken Head");
+            temp.put("lat","24.181797");
+            temp.put("long","120.644607");
+            restartant_info.add(1,temp);
 
+            temp = new HashMap<String,String>();
+            temp.put("name","Cafe Buddha 佈達咖啡");
+            temp.put("lat","24.17674199999999");
+            temp.put("long","120.645502");
+            restartant_info.add(2,temp);
 
-            restartant_info.put("name"+"2","無名麻辣鴨血 逢甲店");
-            restartant_info.put("descr"+"2","鴨血");
-            restartant_info.put("lat"+"2","24.181244");
-            restartant_info.put("long"+"2","120.646336");
+            temp = new HashMap<String,String>();
+            temp.put("name","Yixin Vegetarian Stinky Tofu");
+            temp.put("lat","24.1778562");
+            temp.put("long","120.6451825");
+            restartant_info.add(3,temp);
 
+            temp = new HashMap<String,String>();temp.put("name","Mr.38 The Beat Curry");
+            temp.put("lat","24.1769469");
+            temp.put("long","120.6433179");
+            restartant_info.add(4,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","樂樂和風食堂-台中青海店");
+            temp.put("lat","24.171686");
+            temp.put("long","120.643981");
+            restartant_info.add(5,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","官芝霖大腸包小腸");
+            temp.put("lat","24.1788954");
+            temp.put("long","120.6456488");
+            restartant_info.add(6,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","In Sky Hotel");
+            temp.put("lat","24.1828703");
+            temp.put("long","120.6450211");
+            restartant_info.add(7,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","梅香小吃");
+            temp.put("lat","24.180263");
+            temp.put("long","120.645765");
+            restartant_info.add(8,temp);
+
+            temp = new HashMap<String,String>();temp.put("name","陶板屋 台中河南店");
+            temp.put("lat","24.1715678");
+            temp.put("long","120.6447569");
+            restartant_info.add(9,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","Plant Plan");
+            temp.put("lat","24.1711714");
+            temp.put("long","120.6476609");
+            restartant_info.add(10,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","联亭泡菜鍋-逢甲店");
+            temp.put("lat","24.1827307");
+            temp.put("long","120.6447184");
+            restartant_info.add(11,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","三號創意料理廚房");
+            temp.put("lat","24.1765332");
+            temp.put("long","120.6446064");
+            restartant_info.add(12,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","激旨燒き鳥Gekiuma Yakitori 台灣總店");
+            temp.put("lat","24.1831289");
+            temp.put("long","120.6466054");
+            restartant_info.add(13,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","龍涎居雞膳食坊 台中逢甲店");
+            temp.put("lat","24.1795836");
+            temp.put("long","120.6451754");
+            restartant_info.add(14,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","長谷川壽司專賣店");
+            temp.put("lat","24.1715984");
+            temp.put("long","120.64594");
+            restartant_info.add(15,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","台南鍋燒麵");
+            temp.put("lat","24.1755365");
+            temp.put("long","120.6449622");
+            restartant_info.add(16,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","鍋神日式涮涮鍋");
+            temp.put("lat","24.1835551");
+            temp.put("long","120.6446369");
+            restartant_info.add(17,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","韓石館");
+            temp.put("lat","24.1767214");
+            temp.put("long","120.6426575");
+            restartant_info.add(18,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","月島文字燒 (台中逢甲店)");
+            temp.put("lat","24.174742");
+            temp.put("long","120.6468268");
+            restartant_info.add(19,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","七味厨坊");
+            temp.put("lat","24.179503");
+            temp.put("long","120.646416");
+            restartant_info.add(20,temp);
+
+            temp = new HashMap<String,String>();
+            temp.put("name","無名麻辣鴨血 逢甲店");
+            temp.put("lat","24.181244");
+            temp.put("long","120.646336");
+            restartant_info.add(21,temp);
         }
-
     }
 
     @Override
@@ -82,15 +195,20 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        Log.d("MapsFragmet","list size:"+restartant_info.size());
+
         // Add a marker in Sydney and move the camera
         LatLng FengChia = new LatLng(24.178843,120.646538);
         BitmapDescriptor descriptor = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE);    //設置標記點
         mMap.addMarker(new MarkerOptions().position(FengChia).title("逢甲大學").icon(descriptor).snippet("逢甲大學的地標"));                  //.icon 加入標記點資訊
-        for(int i=0;i<restartant_info.size()/4;i++){
-            LatLng restartant = new LatLng(Double.parseDouble(restartant_info.get("lat"+i)),Double.parseDouble(restartant_info.get("long"+i)));
-            String name = restartant_info.get("name"+i);
-            String descr = restartant_info.get("descr"+i);
-            mMap.addMarker(new MarkerOptions().position(restartant).title(name).icon(descriptor).snippet(descr));
+        for(int i=0;i<restartant_info.size();i++){
+            HashMap<String,String> temp = restartant_info.get(i);
+            Double lat = Double.parseDouble(temp.get("lat"));
+            Double lng = Double.parseDouble(temp.get("long"));
+            LatLng restartant = new LatLng(lat,lng);
+            String name = temp.get("name");
+            Log.d("MapsFragmet","i:"+i+" name:"+name + " lat:"+lat +" lng:"+lng);
+            mMap.addMarker(new MarkerOptions().position(restartant).title(name).icon(descriptor));
         }
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(FengChia));
